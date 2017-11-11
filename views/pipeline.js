45324bff61363d0f19844197f41d4c16fdcfa6a9
Anirudh <askakdagr8@gmail.com>
2017-11-11 11:43:43 -0600

    working on more info modal

--- views/pipeline.js
+++ views/pipeline.js
@@ -9,7 +9,7 @@ $("#generateCards").on("click",function(event){
 		for(var i = 0; i<data.length;i++){
 			if(data[i]["status"] == "contacted"){
 				xAxis1 = xAxis1 + 60;
-				$(".flashCards").append("<div id='yes-drop' class='draggable drag-drop' value="+data[i]["child_first_name"]+data[i]["child_last_name"]+" style='float:left;margin-top:-645px;margin-left:"+xAxis1+"px'"+">"+data[i]["child_first_name"]+" "+data[i]["child_last_name"]);
+				$(".flashCards").append("<div id='yes-drop' class='draggable drag-drop' rel='modal:open' value="+data[i]["child_first_name"]+data[i]["child_last_name"]+" style='float:left;margin-top:-645px;margin-left:"+xAxis1+"px'"+">"+data[i]["child_first_name"]+" "+data[i]["child_last_name"]);
 			}
 			if(data[i]["status"] == "new-lead"){
 				xAxis = xAxis + 60;
@@ -38,6 +38,10 @@ $("#schoolSelect").change(function(event){
 });
 
 
+$(document).on('dblclick', '.drag-drop', function() {
+    console.log("inside dbl");
+    
+});
 
 
 
