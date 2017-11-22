98f40f794df95e795ae5235acf13cba53ad651e1
Anirudh <askakdagr8@gmail.com>
2017-11-22 09:52:22 -0600

    updating

--- config/database.js
+++ config/database.js
@@ -3,7 +3,7 @@ module.exports = {
     'connection': {
         'host': 'localhost',
         'user': 'root',
-        'password': ''
+        'password': 'Cricket47'
     },
 	'database': 'my_schema',
     'users_table': 'users'
